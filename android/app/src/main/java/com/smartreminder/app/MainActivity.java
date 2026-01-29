1b99b6c49802107d19f38317e0f4dc2ebb9a4e3f
Furdui Sorana <furdui.sorana@gmail.com>
2026-01-29 23:41:28 +0200

    fix voice recognition plugin

--- android/app/src/main/java/com/smartreminder/app/MainActivity.java
+++ android/app/src/main/java/com/smartreminder/app/MainActivity.java
@@ -3,15 +3,7 @@ package com.smartreminder.app;
 import android.content.Intent;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.community.speechrecognition.SpeechRecognition;
-
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        registerPlugin(SpeechRecognition.class);
-    }
-
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
